642d443a13b1cb91270dbed7a006c81543c99cbe
ewt <36795362+dverdonschot@users.noreply.github.com>
2024-04-28 08:17:03 +0200

    added homemanager again

--- hosts/media-server/configuration.nix
+++ hosts/media-server/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #<home-manager/nixos>
+      <home-manager/nixos>
     ];
 
   # Use the GRUB 2 boot loader.
