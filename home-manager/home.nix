b09e221f511e2a1745ca09074e357d7a0a626a4a
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-06-09 08:59:50 +0200

    added home-manager for fedora OS in a flake

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -123,4 +123,15 @@
         }
       ];
     };
+    programs.direnv.enable = true;
+    programs.htop = {
+      enable = true;
+      settings = {
+        left_meters = [ "LeftCPUs2" "Memory" "Swap" ];
+        left_right = [ "RightCPUs2" "Tasks" "LoadAverage" "Uptime" ];
+        setshowProgramPath = false;
+        treeView = true;
+      };
+    };
+    programs.jq.enable = true;
 }
---
+++ nix-any-os-home-manager/flake.nix
@@ -0,0 +1,36 @@
+{
+  # based on https://github.com/jonringer/nixpkgs-config/blob/master/flake.nix
+  description = "Home-manager configuration";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, home-manager, nixpkgs, utils }:
+    let
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
+        modules = [ (import ../home-manager/home.nix) ] ++ (args.modules or []);
+        pkgs = pkgsForSystem (args.system or "x86_64-linux");
+      });
+
+    in utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system: rec {
+      legacyPackages = pkgsForSystem system;
+  }) // {
+    # non-system suffixed items should go here
+    nixosModules.home = import ./home.nix; # attr set or list
+
+    homeConfigurations.fedora = mkHomeConfiguration {
+    };
+
+    inherit home-manager;
+    inherit (home-manager) packages;
+  };
+}
