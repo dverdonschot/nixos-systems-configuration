5cbd7525e03a73081506cf33056b34f4f72b1520
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-07-18 14:21:58 +0200

    removing homepage, adding again as module

--- hosts/media-server/configuration.nix
+++ hosts/media-server/configuration.nix
@@ -272,7 +272,7 @@
           defaultNetwork.settings.dns_enabled = true;
        };
       };
-	  virtualisation.oci-containers.containers = {
+	    virtualisation.oci-containers.containers = {
         dashy = {
           image = "lissy93/dashy";
           ports = ["0.0.0.0:8080:8080"];
@@ -310,121 +310,7 @@
   };
 ### dashy
 
-### homepage-dashboard
 
-  containers.homepage = {
-    autoStart = true;
-    enableTun = true;
-    privateNetwork = true;
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.12";
-    bindMounts = {
-      "/films" = {
-        hostPath = "/mnt/films";
-      };
-    };
-
-
-    config = { pkgs, ... }: {
-      environment.systemPackages = with pkgs; [
-        vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-        wget
-        iputils
-        git
-        bind
-        jq
-        zip
-        openssl
-      ];
-
-      services.homepage-dashboard = {
-        enable = true;
-        listenPort = 8082;
-        openFirewall = true;
-        settings = {
-          layout = {
-            Media = {
-              style = "row";
-              columns = 4;
-            };
-          };
-        };
-        bookmarks = [
-          {
-            Developer = [
-              {
-                Github = [
-                  {
-                    abbr = "GH";
-                    href = "https://github.com/";
-                  }
-                ];
-              }
-            ];
-          }
-          {
-            Entertainment = [
-              {
-                YouTube = [
-                  {
-                    abbr = "YT";
-                    href = "https://youtube.com/";
-                  }
-                ];
-              }
-            ];
-          }
-        ];
-        services = [
-          {
-            "My First Group" = [
-              {
-                "My First Service" = {
-                  description = "Homepage is awesome";
-                  href = "http://localhost/";
-                };
-              }
-            ];
-          }
-          {
-            "My Second Group" = [
-              {
-                "My Second Service" = {
-                  description = "Homepage is the best";
-                  href = "http://localhost/";
-                };
-              }
-            ];
-          }
-        ];
-      };
-      services.tailscale = {
-        enable = true;
-        # permit caddy to get certs from tailscale
-        permitCertUid = "caddy";
-      };
-      
-
-
-      services.caddy = {
-        enable = true;
-        extraConfig = ''
-
-          homepage.tail5bbc4.ts.net {
-            reverse_proxy localhost:8082
-          }
-
-        '';
-      };
-
-
-      # open https port
-      networking.firewall.allowedTCPPorts = [ 443 ];
-
-      system.stateVersion = "23.05";
-
-    };
-  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
