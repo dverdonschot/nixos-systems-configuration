804634db47be074d73b67e6c0113197098b999af
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-07-18 18:11:56 +0200

    adding jellyfin to a seperate container

--- nix-containers/jellyfin-container.nix
+++ nix-containers/jellyfin-container.nix
@@ -49,20 +49,6 @@ in {
           jellyfin-web
         ];
 
-      # 1. enable vaapi on OS-level
-      nixpkgs.config.packageOverrides = pkgs: {
-        vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-      };
-      hardware.opengl = {
-        enable = true;
-        extraPackages = with pkgs; [
-          intel-media-driver
-          vaapiIntel
-          vaapiVdpau
-          libvdpau-va-gl
-          intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-        ];
-        }  # 1. enable vaapi on OS-level
         nixpkgs.config.packageOverrides = pkgs: {
           vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
         };
