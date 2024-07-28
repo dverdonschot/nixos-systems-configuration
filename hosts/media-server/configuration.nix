853ae54b5c17367e76722f414d55f7ba7d7883ea
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-07-28 20:25:04 +0200

    fixing bugs

--- hosts/media-server/configuration.nix
+++ hosts/media-server/configuration.nix
@@ -81,6 +81,8 @@
     ];
   };
 
+  security.sudo.wheelNeedsPassword = false;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
