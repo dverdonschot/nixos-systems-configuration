13137fa92e7776503df2664d2ce888cc404f44b0
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-08-18 21:09:28 +0200

    added iperf3

--- hosts/media-server/configuration.nix
+++ hosts/media-server/configuration.nix
@@ -144,11 +144,12 @@
         workstation = true;
       };
   };
+
   services.iperf3 = {
     enable = true;
     openFirewall = true;
     port = 7575;
-  }
+  };
 
 #  services.jellyfin = {
 #    enable = true;
