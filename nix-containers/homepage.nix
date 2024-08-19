7a15c4e5b979eab101deb47edbdc5736ab5059f5
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-08-19 22:15:37 +0200

    added ;

--- nix-containers/homepage.nix
+++ nix-containers/homepage.nix
@@ -156,7 +156,7 @@ in {
                     href = "https://arthurtube.${cfg.tailNet}";
                   }
                 }
-              ]
+              ];
             }
           ];
         };
