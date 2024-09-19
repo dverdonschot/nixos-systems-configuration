65482f80b6b854331bf2445b0f25628e4c777dad
Dennis Verdonschot <36795362+dverdonschot@users.noreply.github.com>
2024-09-19 08:10:46 +0200

    nix on laptop

--- hosts/laptop76/configuration.nix
+++ hosts/laptop76/configuration.nix
@@ -2,74 +2,31 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ lib, config, pkgs, modulesPath, ... }:
-  # nix-software-center requirements
-let
-  nix-software-center = import (pkgs.fetchFromGitHub {
-    owner = "snowfallorg";
-    repo = "nix-software-center";
-    rev = "0.1.2";
-    sha256 = "xiqF1mP8wFubdsAQ1BmfjzCgOD3YZf7EGWl9i69FTls=";
-  }) {};
-in
-
+{ config, pkgs, ... }:
 
 {
   imports =
-    [
-      #"${modulesPath}/profiles/minimal.nix"
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
-      ./user.nix
-#      ./home.nix
     ];
-
-  nixpkgs.overlays = [
-  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = true;
-
-  # trying to prevent nixos rebuild issue where this service fails
-  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # prepare network for finding printer and autodetection in general
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -91,21 +48,17 @@ in
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -113,40 +66,33 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ewt = {
     isNormalUser = true;
     description = "ewt";
-    shell = pkgs.bashInteractive;
-    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
+    extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox
+    #  thunderbird
     ];
   };
 
-  security.sudo.extraRules = [
-    { 
-      users = [ "ewt" ];
-      commands = [
-        { command = "ALL" ;
-          options = [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
-
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "ewt";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  # Install firefox.
+  programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+<<<<<<< HEAD
   
   # Enable nix flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -182,79 +128,17 @@ in
   programs.virt-manager.enable = true;
   services.spice-vdagentd.enable = true;
 
+=======
+>>>>>>> e6e4eea (nix on laptop)
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    nmap
-    git
-    tree
-    gnome.gnome-tweaks
-    gnome.gnome-terminal
-    btop
-    fzf
-    unzip
-    pciutils
-    dpkg
-    tmux
-    wl-clipboard
-    libstdcxx5 ## fooocus
-    libz
-    lshw
-    nix-software-center
-    nix-index
-    vscode-fhs
-    #images
-    pinta
-    tiv
-    # ai
-    ollama
-    # microcrontrollers
-    pciutils
-    usbutils
-    qmk
-    esptool
-    esphome
-    arduino
-    haskellPackages.udev
-    # cli bautification
-    oh-my-posh
-    oh-my-git
-    hack-font
-    powerline-fonts
-    font-awesome
-    # compiling
-    stdenv.cc.cc
-    gcc
-    gcc-unwrapped
-    pkg-config
-    gnumake
-    pkgsCross.avr.buildPackages.gcc
-    appimage-run
-    podman
-    podman-tui
-    podman-compose
-    lazydocker
-    # virtualization
-    virt-manager
-    virt-viewer
-    spice spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-    quickemu
-    quickgui
-    gnome.adwaita-icon-theme
-    # services
-    tailscale
-    awscli2
-    s3fs
-    steam 
-    octoprint
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   ];
 
+<<<<<<< HEAD
   fonts.fontDir.enable = true; 
   fonts.packages = with pkgs; [ pkgs.hack-font ]; 
   fonts.fontconfig = {
@@ -303,8 +187,26 @@ in
   environment.sessionVariables.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   #environment.sessionVariables.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.libudev-zero}/lib/:$LD_LIBRARY_PATH";
   environment.sessionVariables.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH";
+=======
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+>>>>>>> e6e4eea (nix on laptop)
 
   # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -312,6 +214,6 @@ in
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
