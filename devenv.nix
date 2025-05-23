10f99fe1b9353f33e0646edec7454bb3fe7635bc
Dennis Verdonschot <36795362+dverdonschot@users.noreply.github.com>
2025-05-23 07:15:24 +0200

    new flake update

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.vscode ];
+  packages = [ pkgs.git ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
