ab4e674b9c52c82d78226da06b2e75e5c8ddd0d1
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2026-05-17 22:58:49 +0200

    Update home-minimal.nix

--- home-manager/home-minimal.nix
+++ home-manager/home-minimal.nix
@@ -27,6 +27,8 @@
       #defaultEditor = true;
       viAlias = true;
       vimAlias = true;
+      withRuby = false;
+      withPython3 = true;
       plugins = with pkgs.vimPlugins; [
         dracula-nvim
         nerdtree
