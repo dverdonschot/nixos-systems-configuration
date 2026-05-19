bf3c6e2368b2ca6559b76cce934a359d4a47920d
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2026-05-19 21:28:54 +0200

    Replace Technitium DNS module with Unbound DNS module

--- hosts/odroid/configuration.nix
+++ hosts/odroid/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../modules/technitium-dns.nix
+      ../../modules/unbound-dns.nix
       #<home-manager/nixos>
       ../../nix-containers/search-container.nix
       ../../nix-containers/forgejo-container.nix
