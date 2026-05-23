72186adf269335f148aa732e29c712fc5f9d8820
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2026-05-23 14:57:40 +0200

    Refactor Tailscale ethtool service definition

--- modules/persistant-udp-gro-tailscale.nix
+++ modules/persistant-udp-gro-tailscale.nix
@@ -1,11 +1,15 @@
-systemd.services.tailscale-ethtool = {
-  description = "Optimize UDP forwarding for Tailscale subnet routing";
-  wantedBy = [ "multi-user.target" ];
-  after = [ "network-online.target" ];
-  wants = [ "network-online.target" ];
+{ pkgs, ... }:
 
-  serviceConfig = {
-    Type = "oneshot";
-    ExecStart = "${pkgs.ethtool}/bin/ethtool -K br0 rx-udp-gro-forwarding on rx-gro-list off";
+{
+  systemd.services.tailscale-ethtool = {
+    description = "Optimize UDP forwarding for Tailscale subnet routing";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ethtool}/bin/ethtool -K br0 rx-udp-gro-forwarding on rx-gro-list off";
+    };
   };
-};
+}
