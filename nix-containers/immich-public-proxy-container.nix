ebb87de4a657de5cfe1b164bf0a6a9ee0adf10b8
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2026-07-10 22:12:51 +0200

    Rename homepage service to immich-public-proxy

---
+++ nix-containers/immich-public-proxy-container.nix
@@ -0,0 +1,119 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.services.immich-public-proxy;
+in {
+  options.services.immich-public-proxy = {
+    enable = mkEnableOption "Enable immich-public-proxy service";
+    tailNet = mkOption {
+      type = types.str;
+      default = "tail1abc2.ts.net";
+    };
+    containerName = mkOption {
+      type = types.str;
+      default = "immich-public-proxy";
+    };
+    ipAddress = mkOption {
+      type = types.str;
+      default = "192.168.100.12";
+    };
+    hostAddress = mkOption {
+      type = types.str;
+      default = "192.168.100.10";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Option definitions.
+    # Define what other settings, services and resources should be active.
+    # Usually these depend on whether a user of this module chose to "enable" it
+    # using the "option" above.
+    # Options for modules imported in "imports" can be set here.
+
+    containers.${cfg.containerName} = {
+      autoStart = true;
+      enableTun = true;
+      privateNetwork = true;
+      hostAddress = "${cfg.hostAddress}";
+      localAddress = "${cfg.ipAddress}";
+
+      extraFlags = [ "--private-users-ownership=chown" ];
+      additionalCapabilities = [
+        # This is a very ugly hack to add the system-call-filter flag to
+        # nspawn. extraFlags is written to an env file as an env var and
+        # does not support spaces in arguments, so I take advantage of
+        # the additionalCapabilities generation to inject the command
+        # line argument.
+        ''all" --system-call-filter="add_key keyctl bpf" --capability="all''
+      ];
+      allowedDevices = [
+        { node = "/dev/fuse"; modifier = "rwm"; }
+        { node = "/dev/mapper/control"; modifier = "rw"; }
+        { node = "/dev/console"; modifier = "rwm"; }
+      ];
+      bindMounts.fuse = {
+        hostPath = "/dev/fuse";
+        mountPoint = "/dev/fuse";
+        isReadOnly = false;
+      };
+
+      config = { config, pkgs, ... }: {
+        boot.isContainer = true;
+        systemd.services.docker.path = [ pkgs.fuse-overlayfs ];
+
+        environment.systemPackages = with pkgs; [
+          vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+          wget
+          iputils
+          git
+          bind
+          jq
+          zip
+          openssl
+        ];
+
+        services.journald.extraConfig = "SystemMaxUse=100M";
+        virtualisation.docker = {
+          enable = true;
+          autoPrune.enable = true;
+          rootless = {
+            enable = true;
+            setSocketVariable = true;
+          };
+        };
+
+        services.immich-public-proxy = {
+          enable = true;
+          port = 3000;
+          immichUrl = "immich.${cfg.tailNet}";
+          openFirewall = false;
+        };
+        services.tailscale = {
+          enable = true;
+          # permit caddy to get certs from tailscale
+          permitCertUid = "caddy";
+        };
+
+
+
+        services.caddy = {
+          enable = true;
+          extraConfig = ''
+
+            immich-public-proxy.${cfg.tailNet} {
+              reverse_proxy localhost:3000
+            }
+
+          '';
+        };
+
+
+        # open https port
+        networking.firewall.allowedTCPPorts = [ 443 ];
+
+        system.stateVersion = "25.05";
+
+      };
+    };
+  };
+}
