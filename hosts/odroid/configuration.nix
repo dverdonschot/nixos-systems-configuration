d12a2e2932d4fc1bc3aed885319a5dbb14084622
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2026-07-26 13:57:26 +0200

    reset to previous commit

--- hosts/odroid/configuration.nix
+++ hosts/odroid/configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+p# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -454,12 +454,6 @@
     bind = null;
     port = 6379;
     openFirewall = true;
-  };
-
-  services.duplicati = {
-    enable = true;
-    interface = "any";
-    user = "root";
   };
 
 #  services.caddy = {
